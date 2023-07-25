a87805047e97dca7279b34a53c71679b2fa095d2
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-07-25 16:22:10 +0000

    added more css

--- src/js/component/navBar.jsx
+++ src/js/component/navBar.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const NavBar = () =>{
-    return (
-        <>
-        <nav className="navbar navbar-expand-lg bg-secondary">
-        <div className="container" >
-          <div className="navbar-left" style={{float:" left"}}>
-            <a className="navbar-brand" href="#">Start Bootstrap</a>
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-secondary">
+        <div className="container">
+          <div className="navbar-left">
+            <a className="navbar-brand" href="#">
+              Start Bootstrap
+            </a>
             <button
-              className="navbar-toggler"
+              className="navbar-toggler "
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarNav"
@@ -16,34 +18,43 @@ const NavBar = () =>{
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span className="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon "></span>
             </button>
           </div>
-          <div className="navbar-right" style={{float:"right"}}>
+          <div className="navbar-right d-flex float end" >
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link " aria-current="page" href="#" id="Home"
-                    >Home</a>
-                  
+                  <a
+                    className="nav-link "
+                    aria-current="page"
+                    href="#"
+                    id="Home"
+                  >
+                    Home
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">About</a>
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Services</a>
+                  <a className="nav-link" href="#">
+                    Services
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Contact</a>
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
       </nav>
-  
-        </>
-    );
-
+    </>
+  );
 };
 export default NavBar;
